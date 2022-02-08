745e6c66cbf72eb085e6556849faaf66d2238015
omar saade <omarsaadeh11@gmail.com>
2022-02-08 23:28:27 +0200

    todo/local storage/tasks existence

--- todol.js
+++ todol.js
@@ -1,13 +1,16 @@
 /*
   Students Tasks:
-  [1] Use Sweet Alert If Input Is Empty
+  [1] Use Sweet Alert If Input Is Empty//
   [2] Check If Task Is Exist
   [3] Create Delete All Tasks Button
   [4] Create Finish All Tasks Button
-  [5] Add To Tasks To The Local Storage
+  [5] Add The Tasks To The Local Storage
 */
 
 // Setting Up Variables
+
+
+// import 'sweetalert2/src/sweetalert2.scss';
 let theInput = document.querySelector(".add-task input");//  input
 let theAddButton = document.querySelector(".add-task .plus"); // add button
 let tasksContainer = document.querySelector(".tasks-content");// task li btezhar fi text el maktube
@@ -24,8 +27,12 @@ theAddButton.onclick = function () {
 
     // If Input is Empty
     if (theInput.value === '') {
+        Swal.fire(
+            'Please Enter Something!',
 
-        console.log("No Value");
+        )
+
+
 
     } else {
 
@@ -39,6 +46,12 @@ theAddButton.onclick = function () {
             noTasksMsg.remove();
 
         }
+
+
+        //  if () {
+
+        //  }
+
 
         // Create Main Span Element
         let mainSpan = document.createElement("span");
@@ -70,6 +83,10 @@ theAddButton.onclick = function () {
         // Add The Task To The Container
         tasksContainer.appendChild(mainSpan);
 
+
+
+        localStorage.setItem('myCat', [theInput.value]);
+
         // Empty The Input
         theInput.value = '';
 
@@ -78,10 +95,67 @@ theAddButton.onclick = function () {
 
         // Calculate Tasks
         calculateTasks();
+        // console.log(document.querySelectorAll('.tasks-content .task-box'));
+
+        var mvp = Array.from(document.querySelectorAll('.tasks-content .task-box'));
+
+
+        // console.log(mvp.childNodes);
+        // for (let i = 0; i < mvp.length; i++) {
+        //     var box = mvp[i].firstChild.data;
+
+
+
+        //     // console.log(box);
+
+
+        // }
+        // console.log(box);
+
+        // console.log(box);
+
+
+
+
+        // hasDuplicates(mvp);
+        // console.log(hasDuplicates(mvp));
+
+
+        // console.log(mvp[0].firstChild.data);
+        // console.log(mvp[1].firstChild.data);
+        // if (mvp.length > 1) {
+        //     for (let i = 0; i < mvp.length; i++) {
+
+        //         for (let j = 1; j < mvp.length; j++) {
+        //             if (mvp[i].firstChild.data === mvp[j].firstChild.data) {
+        //                 Swal.fire(
+        //                     'Task Is Exist!',
+
+        //                 )
+        //             }
+        //         }
+
+
+        //     }
+        // }
+
+
+
+
+
+
+        // if (mvp[0].firstChild.data === mvp[1].firstChild.data) {
+        //     console.log("good");
+        // }
+
+
+
+
 
     }
 
 };
+
 
 document.addEventListener('click', function (e) {
 
@@ -110,6 +184,7 @@ document.addEventListener('click', function (e) {
 
     // Calculate Tasks
     calculateTasks();
+
 
 });
 
@@ -141,5 +216,61 @@ function calculateTasks() {
 
     // Calculate Completed Tasks
     tasksCompleted.innerHTML = document.querySelectorAll('.tasks-content .finished').length;
+
+
+
+}
+
+// var c = ["ali", "sami", "ahmad", "ali"]
+
+// function hasDuplicates(array) {
+//     return (new Set(array)).size !== array.length;
+// }
+// hasDuplicates(c);
+// console.log(hasDuplicates(c));
+
+
+// var deleleto = document.getElementById("del");
+
+// deleleto.onclick = function () {
+//     // document.querySelector(".task-box").outerHTML = "";
+//     // console.log(document.querySelectorAll(".task-box"));
+//     // document.querySelector(".tasks-content").childNodes
+//     // childElementCount;
+
+// }
+
+
+document.getElementById("del").onclick = function () {
+    var node = document.getElementById("no");
+    node.parentNode.removeChild(node);
+    location.reload();
+
+
+}
+
+document.getElementById("fin").onclick = function () {
+
+
+    var node = document.querySelector(".task-box");
+
+    // node.parentNode.removeChild(node);
+
+
+    node.parentNode.classList.add("showOverlay");
+    // node.parentNode.classList.remove("showOverlay");
+    // document.querySelector(".no-tasks-message").classList.add("ho");
+    console.log(document.querySelector(".no-tasks-message"));
+
+
+    setInterval(() => {
+        location.reload();
+    }, 2000);
+
+    // var node = document.getElementById("no");
+
+
+
+
 
 }
